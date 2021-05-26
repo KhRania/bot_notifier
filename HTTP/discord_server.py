9e6fc49a9c3024fc41d2dc4863f807a2a409507f
Rania khadaier <rania.khadaier@u-become.com>
2021-05-26 17:32:42 +0100

    final update of modules

--- HTTP/discord_server.py
+++ HTTP/discord_server.py
@@ -8,159 +8,11 @@ import schedule
 import time
 import datetime
 from keep_alive import keep_alive
+from test import notificationHook
+
+
 
 client = discord.Client()
-
-
-#Test if the connection to /state is available
-#urls contains the url of state from REST Server
-url=cfg.urls["stateurl"]
-#The result of this test will be stored in scheduleRequest & state_request to do the treatment afterwards
-def getUrl():
-    robot_status_list=[]
-    notification_msg=''
-    color_msg=''
-    state_request=False
-    if url :
-      try:
-        response = requests.get(url)
-
-      # If the response was successful, no Exception will be raised
-        response.raise_for_status()
-      except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-      except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-      #No Exception raised => Connection to the Rest Server with success    
-      else:
-                      
-        print('Success Connection To Rest Server!')
-        state_request=True
-           
-    if (state_request) :     
-                    # extracting state data in json format
-        list_state=requests.get(url).json()
-
-        calendar_image='https://img.icons8.com/color/48/000000/planner.png'
-                    # extracting state data in json format
-                  
-                    
-        list_state=requests.get(cfg.urls["stateurl"]).json()
-                  
-                    #State MSG
-        robot_state=':ballot_box_with_check:'
-        humidity_state=':ballot_box_with_check:'
-        temperature_state=':ballot_box_with_check:'
-        battery_state=':ballot_box_with_check:'
-        free_disk_percentage_state=':ballot_box_with_check:'
-                    #normal color msg blue
-        color_msg=0x5CDBF0
-                    #Robot status
-        robot_status=list_state['state']
-        robot_status_list.append(robot_status)
-                    #Temperature Value
-        temperature_value=list_state['sensors']['temperature']
-                    #Temperature Message send with the notification is empty when temperature_value is normal
-        temperature_msg=''
-                    #humidity Value
-        humidity_value=list_state['sensors']['humidity']
-                    #humidity Message send with the notification is empty when humidity_value is normal
-        humidity_msg=''
-                    # battery percentage
-        battery_percentage=round(float(list_state['battery']['percentage']*100),1)
-                    #battery Message send with the notification is empty when battery_percentage is normal
-        battery_msg=''
-                    #Free Disk Storage percentage
-        free_disk_percentage=round((float(list_state['storage']['free'])/float(list_state['storage']['total']))*100,1)
-                    #Total Disk Storage percentage
-        total_percentage=round(float(list_state['storage']['total']),1)
-                    #free_disk Message send with the notification is empty when free_disk_percentage is normal
-        free_disk_msg=''
-                    
-                  
-                    # Warning case if battery value < 0.2 msg for battery status switch to warning
-        if list_state['battery']['percentage']< cfg.config['Settings']['battery']:
-          battery_state='⚠️'
-          color_msg=0xFF8800
-          battery_msg=' below  '+str(round(cfg.config['Settings']['battery']*100))+'%'
-
-                    # Warning case if free_disk_percentage < 20 % msg for battery status switch to warning
-        if free_disk_percentage < round(cfg.config['Settings']['disk']*100,1):
-          free_disk_percentage_state='⚠️'
-          color_msg=0xFF8800
-          free_disk_msg=' below '+str(round(cfg.config['Settings']['disk']*100))+'%'
-
-                    #Warning Temperature 
-        if temperature_value <= cfg.config['Settings']['temperature']['min']:
-          temperature_state='⚠️'
-          color_msg=0xFF8800
-          temperature_msg='below '+str(int(cfg.config['Settings']['temperature']['min']))+'°C'
-                      
-        elif temperature_value >= cfg.config['Settings']['temperature']['max']:
-          temperature_state='⚠️'
-          color_msg=0xFF8800
-          temperature_msg='above '+str(int(cfg.config['Settings']['temperature']['max']))+'°C'
-                        
-                    #Warning Humidity
-        if humidity_value <= cfg.config['Settings']['humidity']['min']:
-          humidity_state='⚠️'
-          color_msg=0xFF8800
-          humidity_msg='below '+str(int(cfg.config['Settings']['humidity']['min']))+'%'
-        elif humidity_value >= cfg.config['Settings']['humidity']['max']:
-          humidity_state='⚠️'
-          color_msg=0xFF8800
-          humidity_msg='above '+str(int(cfg.config['Settings']['humidity']['max']))+'%'
-
-                    #Get the date and time from local to check with event in get request of schedule
-                    #startDateNow= datetime.datetime.today().strftime('%Y-%m-%d')
-                    #startTimeNow= datetime.datetime.now().time().strftime("%H:%M")
-                  
-                    # Test the Status if it FAILED or EMERGENCY the msg of th robot state is failed and the color is red 
-        if list_state['state'].lower() == 'failure' or list_state['state'].lower() == 'emergency':
-          robot_state='⚠️'
-          color_msg=0xF04747
-
-                    #test the length of the array to send the first notification     
-        if (len(robot_status_list)==1):
-            if(len(list_state['current_events'])==1):
-                            
-                            # utc = datetime.utcnow()
-              end_time_event_utc = datetime.datetime.strptime(list_state['current_events'][0]['stop'], '%Y-%m-%d %H:%M:%S')
-              local_time= end_time_event_utc.time()
-              now_timestamp = time.time()
-              offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
-              end_time_event_local =str((datetime.datetime.combine(datetime.date(1,1,1),local_time) + offset).time())
-              notification_msg=robot_state+' : Next start '+'**'+robot_status+'**'+' now end '+end_time_event_local[:5]+'\n'+'\n'+battery_state+' : Battery state :battery: '+str(battery_percentage)+'% '+battery_msg+'\n'+'\n'+free_disk_percentage_state+' : Free storage :cd: '+str(free_disk_percentage)+'% '+free_disk_msg+'\n'+'\n'+temperature_state+' : Temperature :thermometer: '+str(temperature_value)+'°C '+temperature_msg+'\n'+'\n'+humidity_state+' : Humidity :droplet: '+str(humidity_value)+'% '+humidity_msg
-
-                          
-            else:
-              notification_msg=robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'+battery_state+' : Battery state :battery: '+str(battery_percentage)+'% '+battery_msg+'\n'+'\n'+free_disk_percentage_state+' : Free storage :cd: '+str(free_disk_percentage)+'% '+free_disk_msg+'\n'+'\n'+temperature_state+' : Temperature :thermometer: '+str(temperature_value)+'°C '+temperature_msg+'\n'+'\n'+humidity_state+' : Humidity :droplet: '+str(humidity_value)+'% '+humidity_msg
-
-                            
-
-                    #test if the old status is different to the new one in this case the length of the array must be greater than 1    
-        elif ((len(robot_status_list)>1) and (robot_status!=robot_status_list[-2]))  :
-            if(len(list_state['current_events'])==1):
-                            
-                            # utc = datetime.utcnow()
-              end_time_event_utc = datetime.datetime.strptime(list_state['current_events'][0]['stop'], '%Y-%m-%d %H:%M:%S')
-              local_time= end_time_event_utc.time()
-              now_timestamp = time.time()
-              offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
-              end_time_event_local =str((datetime.datetime.combine(datetime.date(1,1,1),local_time) + offset).time())
-                            #Prepare notification content to send with Webhook 
-              notification_msg=robot_state+' : Next start '+'**'+robot_status+'**'+' now end '+end_time_event_local[:5]+'\n'+'\n'+battery_state+' : Battery state :battery: '+str(battery_percentage)+'% '+battery_msg+'\n'+'\n'+free_disk_percentage_state+' : Free storage :cd: '+str(free_disk_percentage)+'% '+free_disk_msg+'\n'+'\n'+temperature_state+' : Temperature :thermometer: '+str(temperature_value)+'°C '+temperature_msg+'\n'+'\n'+humidity_state+' : Humidity :droplet: '+str(humidity_value)+'% '+humidity_msg
-
-            else:
-              notification_msg=robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'+battery_state+' : Battery state :battery: '+str(battery_percentage)+'% '+battery_msg+'\n'+'\n'+free_disk_percentage_state+' : Free storage :cd: '+str(free_disk_percentage)+'% '+free_disk_msg+'\n'+'\n'+temperature_state+' : Temperature :thermometer: '+str(temperature_value)+'°C '+temperature_msg+'\n'+'\n'+humidity_state+' : Humidity :droplet: '+str(humidity_value)+'% '+humidity_msg
-                            
-                      
-                        #to get only the last 2 elements of robot_status_list to avoid having an array with unlimited content
-        robot_status_list=robot_status_list[-2:]
-    
-    return notification_msg,color_msg,state_request
-
-
 
 #register an event 
 #discrod.py is an async library to manage thinks it will be by callbacks 
@@ -178,10 +30,7 @@ async def on_message(message):
   if message.author == client.user:
     print('User sends a msg')
   if message.content.startswith('!notify'):
-    #Get the date and time from local to check with event in get request of schedule
-    startDateNow= datetime.datetime.today().strftime('%Y-%m-%d')
-    startTimeNow= datetime.datetime.now().time().strftime("%H:%M")
-    notification,color,status_request=getUrl()
+ 
     if notification and status_request==True:
       embedVar = discord.Embed(title='Date : '+startDateNow+' Time : '+startTimeNow, description=notification ,color=color)   
       await message.channel.send(embed=embedVar)
--- HTTP/test.py
+++ HTTP/test.py
@@ -16,27 +16,13 @@ class notificationHook:
 
     #set robot_status_list as a global array
     robot_status_list=[]
-    #json content of state url
-    #list_state='{}'
     #WebHokk URL Provided By Discord (this url serve to communicate with Discord App)
-    discord_channel=cfg.urls["webhookurl"]
+    discord_channel=Webhook(cfg.urls["webhookurl"])
     #urls contains the url of state from REST Server
     state_url=cfg.urls["stateurl"]
-    #icons 
-    robot_icon=''
-    humidity_icon=''
-    temperature_icon=''
-    battery_icon=''
-    free_disk_percentage_icon=''
-        
-    #Get the date and time from local to check with event in get request of schedule
-    startDateNow= datetime.datetime.today().strftime('%Y-%m-%d')
-    startTimeNow= datetime.datetime.now().time().strftime("%H:%M")
-
     
     def getRequestURL(self):
-      
-        hook= Webhook(self.discord_channel)
+        list_state='{}'
         #The result of this test will be stored in scheduleRequest & state_request to do the treatment afterwards
         state_request=False
         
@@ -53,14 +39,14 @@ class notificationHook:
                 print(f'Other error occurred: {err}')  # Python 3.6
             #No Exception raised => Connection to the Rest Server with success    
             else:
-                
-                print('Success Connection To Rest Server!')
                 state_request=True
                 # extracting state data in json format
                 list_state=requests.get(self.state_url).json()
         return state_request,list_state
 
     def batteryMessage(self):
+        #normal color msg blue
+        color_msg=0x5CDBF0
         state_request,list_state=self.getRequestURL()
         if(state_request):
             # battery percentage
@@ -69,23 +55,22 @@ class notificationHook:
             battery_state=':ballot_box_with_check:'
             #battery Message send with the notification is empty when battery_percentage is normal
             battery_msg=battery_state+' : Battery state :battery: '+str(battery_percentage)+'% '
-            #normal color msg blue
-            color_msg=0x5CDBF0
+         
             # Warning case if battery value < 0.2 msg for battery status switch to warning
             if list_state['battery']['percentage']< cfg.config['Settings']['battery']:
                 battery_state='⚠️'
                 color_msg=0xFF8800
                 battery_msg=battery_state+' : Battery state :battery: '+str(battery_percentage)+'% '+'below  '+str(round(cfg.config['Settings']['battery']*100))+'%'
         
-        return battery_msg
+        return battery_msg,color_msg
     
     def storageMessage(self):
+        #normal color msg blue
+        color_msg=0x5CDBF0
         state_request,list_state=self.getRequestURL()
         if(state_request):
             #icon if value normal
             free_disk_percentage_state=':ballot_box_with_check:'
-            #normal color msg blue
-            color_msg=0x5CDBF0
             #Free Disk Storage percentage
             free_disk_percentage=round((float(list_state['storage']['free'])/float(list_state['storage']['total']))*100,1)
             #free_disk Message send with the notification is empty when free_disk_percentage is normal
@@ -95,15 +80,15 @@ class notificationHook:
                 free_disk_percentage_state='⚠️'
                 color_msg=0xFF8800
                 free_disk_msg=free_disk_percentage_state+' : Free storage :cd: '+str(free_disk_percentage)+'% '+'below '+str(round(cfg.config['Settings']['disk']*100))+'%'
-        return free_disk_msg
+        return free_disk_msg,color_msg
 
     def temperatureMessage(self):
+        #normal color msg blue
+        color_msg=0x5CDBF0
         state_request,list_state=self.getRequestURL()
         if(state_request):
             #icon if value normal
             temperature_state=':ballot_box_with_check:'
-            #normal color msg blue
-            color_msg=0x5CDBF0
             #Temperature Value
             temperature_value=list_state['sensors']['temperature']
             #Temperature Message send with the notification is empty when temperature_value is normal
@@ -118,10 +103,12 @@ class notificationHook:
                 color_msg=0xFF8800
                 temperature_msg=temperature_state+' : Temperature :thermometer: '+str(temperature_value)+'°C above '+str(int(cfg.config['Settings']['temperature']['max']))+'°C'
         
-        return temperature_msg
+        return temperature_msg,color_msg
 
 
     def humidityMessage(self):
+        #normal color msg blue
+        color_msg=0x5CDBF0
         state_request,list_state=self.getRequestURL()
         if(state_request):
             #icon if value normal
@@ -130,8 +117,6 @@ class notificationHook:
             humidity_value=list_state['sensors']['humidity']
             #humidity Message send with the notification is empty when humidity_value is normal
             humidity_msg=humidity_state+' : Humidity :droplet: '+str(humidity_value)+'% '
-            #normal color msg blue
-            color_msg=0x5CDBF0
             #Warning Humidity
             if humidity_value <= cfg.config['Settings']['humidity']['min']:
                 humidity_state='⚠️'
@@ -141,64 +126,125 @@ class notificationHook:
                 humidity_state='⚠️'
                 color_msg=0xFF8800
                 humidity_msg=humidity_state+' : Humidity :droplet: '+str(humidity_value)+'%'+' above '+str(int(cfg.config['Settings']['humidity']['max']))+'%'
-        return humidity_msg
-    
-    def stateMessage(self):
-        robot_status_list=[]
+        return humidity_msg,color_msg
+
+    #Get the date and time from local to check with event in get request of schedule
+    def getNow(self):
+        #Get the date and time from local to check with event in get request of schedule
+        startDateNow= datetime.datetime.today().strftime('%Y-%m-%d')
+        startTimeNow= datetime.datetime.now().time().strftime("%H:%M")
+        return startDateNow,startTimeNow
+
+    #convert time of event in /state
+    def getEventTime(self):
+
+        start_time_event_local = ''
+        end_time_event_local = ''
         state_request,list_state=self.getRequestURL()
         if(state_request):
+            if(len(list_state['current_events'])==1):
+
+                now_timestamp = time.time()
+                offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
+                #time start event
+                start_time_event_utc = datetime.datetime.strptime(list_state['current_events'][0]['start'], '%Y-%m-%d %H:%M:%S')
+                start_local_time = start_time_event_utc.time()
+                start_time_event_local = str((datetime.datetime.combine(datetime.date(1,1,1),start_local_time) + offset).time())
+                start_time_event_local = start_time_event_local[:5]
+                #time end event
+                end_time_event_utc = datetime.datetime.strptime(list_state['current_events'][0]['stop'], '%Y-%m-%d %H:%M:%S')
+                end_local_time = end_time_event_utc.time()
+                end_time_event_local = str((datetime.datetime.combine(datetime.date(1,1,1),end_local_time) + offset).time())
+                end_time_event_local = end_time_event_local[:5]
+            
+            else:
+                pass
+        return start_time_event_local,end_time_event_local
+
+    #convert time of event in /state
+    def getEventDate(self):
         
-            #Get the date and time from local to check with event in get request of schedule
-            startDateNow= datetime.datetime.today().strftime('%Y-%m-%d')
-            startTimeNow= datetime.datetime.now().time().strftime("%H:%M")
+        start_date_event_local  = ''
+        end_date_event_local    = ''
+
+        state_request,list_state=self.getRequestURL()
+        if(state_request):
+            if(len(list_state['current_events'])==1):
+                start_date_event_local  = list_state['current_events'][0]['start'][:11]
+                end_date_event_local    = list_state['current_events'][0]['stop'][:11]
+            else:
+                pass
+        return start_date_event_local,end_date_event_local
+            
+
+
+    def stateMessage(self):
+        battery_msg,colorB=self.batteryMessage()
+        free_disk_msg,colorS=self.storageMessage()
+        temperature_msg,colorT=self.temperatureMessage()
+        humidity_msg,colorH=self.humidityMessage()
+        #normal color msg blue
+        color_msg=0x5CDBF0
+        color_warning=0xFF8800
+        #list color to choose the color of the notification to send
+        for color_warning in [colorB,colorS,colorT,colorH]:
+            color_msg= color_warning
+        #icon calendar to be added in the notification to send
+        calendar=':calendar_spiral:'
+        dateNow,timeNow=self.getNow()
+        startTime,endTime=self.getEventTime()
+        startDate,endDate=self.getEventDate()
+        #State icon for normal state ( ex: AUTOPILOT)
+        robot_state=':ballot_box_with_check:'
+        notification_msg=''
+        state_request,list_state=self.getRequestURL()
+        if(state_request):
             #Robot status
             robot_status=list_state['state']
-            robot_status_list.append(robot_status)
+            self.robot_status_list.append(robot_status)
             # Test the Status if it FAILED or EMERGENCY the msg of th robot state is failed and the color is red 
             if list_state['state'].lower() == 'failure' or list_state['state'].lower() == 'emergency':
                     robot_state='⚠️'
                     color_msg=0xF04747
-
             #test the length of the array to send the first notification     
-            if (len(robot_status_list)==1):
-                if(len(list_state['current_events'])==1):
-                    
-                    # utc = datetime.utcnow()
-                    end_time_event_utc = datetime.datetime.strptime(list_state['current_events'][0]['stop'], '%Y-%m-%d %H:%M:%S')
-                    local_time= end_time_event_utc.time()
-                    now_timestamp = time.time()
-                    offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
-                    end_time_event_local =str((datetime.datetime.combine(datetime.date(1,1,1),local_time) + offset).time())
-
-                    notification_msg=robot_state+' : Next start '+'**'+robot_status+'**'+' now end '+end_time_event_local[:5]+'\n'+'\n'
-                else:
+            if (len(self.robot_status_list)==1):
+                if(startTime and endTime and startDate and endDate):
                     #Prepare notification content to send with Webhook
-                    notification_msg=robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'
-
-             #test if the old status is different to the new one in this case the length of the array must be greater than 1    
-            elif ((len(robot_status_list)>1) and (robot_status!=robot_status_list[-2]))  :
-                if(len(list_state['current_events'])==1):
-                    
-                    # utc = datetime.utcnow()
-                    end_time_event_utc = datetime.datetime.strptime(list_state['current_events'][0]['stop'], '%Y-%m-%d %H:%M:%S')
-                    local_time= end_time_event_utc.time()
-                    now_timestamp = time.time()
-                    offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
-                    end_time_event_local =str((datetime.datetime.combine(datetime.date(1,1,1),local_time) + offset).time())
-                    #Prepare notification content to send with Webhook 
-                    notification_msg=robot_state+' : Next start '+'**'+robot_status+'**'+' now end '+end_time_event_local[:5]+'\n'+'\n'
+                    notification_msg=calendar+' : Start Event '+'**'+startDate+'**'+' Time '+'**'+startTime+'**'+'\n'+'\n'+calendar+' : End Event '+'**'+endDate+'**'+' Time '+'**'+endTime+'**'+'\n'+'\n'+robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'+battery_msg+'\n'+'\n'+free_disk_msg+'\n'+'\n'+temperature_msg+'\n'+'\n'+humidity_msg
                 else:
-                    notification_msg=robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'
+                    notification_msg=calendar+' ** Date :** '+'**'+dateNow+'**'+' **Time ** '+'**'+timeNow+'**'+'\n'+'\n'+robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'+battery_msg+'\n'+'\n'+free_disk_msg+'\n'+'\n'+temperature_msg+'\n'+'\n'+humidity_msg
+
+            #test if the old status is different to the new one in this case the length of the array must be greater than 1    
+            elif ((len(self.robot_status_list)>1) and (robot_status!=self.robot_status_list[-2]))  :
+                if(startTime and endTime and startDate and endDate):
+                    #Prepare notification content to send with Webhook 
+                    notification_msg=calendar+' : Start Event '+'**'+startDate+'**'+' Time '+'**'+startTime+'**'+'\n'+'\n'+calendar+' : End Event '+'**'+endDate+'**'+' Time '+'**'+endTime+'**'+'\n'+'\n'+robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'+battery_msg+'\n'+'\n'+free_disk_msg+'\n'+'\n'+temperature_msg+'\n'+'\n'+humidity_msg
+                else:
+                    notification_msg=calendar+' ** Date :** '+'**'+dateNow+'**'+' **Time ** '+'**'+timeNow+'**'+'\n'+'\n'+robot_state+' : Robot state '+'**'+robot_status+'**'+'\n'+'\n'+battery_msg+'\n'+'\n'+free_disk_msg+'\n'+'\n'+temperature_msg+'\n'+'\n'+humidity_msg
             #to get only the last 2 elements of robot_status_list to avoid having an array with unlimited content
-            robot_status_list=robot_status_list[-2:]        
+            self.robot_status_list=self.robot_status_list[-2:]
+            return notification_msg,color_msg         
+    
+    def postWebhook(self):
+      
+        body_msg,color_msg=self.stateMessage()
+        if body_msg:
+            #Prepare notification content to send with Webhook                
+            notification = Embed(description=body_msg,color=color_msg)       
+            #Send the Notification to Discord
+            self.discord_channel.send(embed=notification)
+            print('Success Connection To Rest Server!')
+        else:
+            print("Nothing to send !")
 
     
 
 
-        
-
-  
 
 if __name__ == "__main__":    
     notif= notificationHook()
-    notif.getRequestURL () 
+    schedule.every(10).seconds.do(notif.postWebhook)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
