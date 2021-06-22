8179d3a08f05eb7efc15419168f48a98cd1e55a8
Rania khadaier <rania.khadaier@u-become.com>
2021-06-22 17:11:33 +0100

    log start/end and test

--- app/configuration.py
+++ app/configuration.py
@@ -3,6 +3,5 @@ urls={
     "settingsurl":"http://127.0.0.1:40054/settings",
     "webhookurl":"https://discord.com/api/webhooks/840274499710615602/v04Tylo1M3Y7l7yNELisaAX7QeRL1HHLp2lumLW9PxhCFsDSbPQEHBR87-nhWRQYdQUb",
     "quad1":"http://151.253.224.74:3008/axis-cgi/jpg/image.cgi?&compression=25&camera=quad",
-    "quad2":"http://151.253.224.74:3004/axis-cgi/jpg/image.cgi?&compression=25&camera=quad",
-    "token":"ODQ1MzE0ODUxNTkyMDc3MzEy.YKfKzQ.7YnhYLEee9QZeE-kZVV9PVfpNZI"
+    "quad2":"http://151.253.224.74:3004/axis-cgi/jpg/image.cgi?&compression=25&camera=quad"
 }
--- app/log_diagnostic.py
+++ app/log_diagnostic.py
@@ -23,6 +23,7 @@ robot_status_list=[]
 
 
 def StartEndEvent():
+
     global robot_status_list
     #get responses from /state 
     state_request,list_state=notif.getResponseState()
@@ -36,21 +37,32 @@ def StartEndEvent():
         #get last two states
         robot_status=list_state['state']
         robot_status_list.append(robot_status)
-        #to get only the last 2 elements of robot_status_list to avoid having an array with unlimited content
-        robot_status_list=robot_status_list[-2:]  
+        # if robot_status_list[0]==robot_status_list[1]:
+        #     robot_status_list=robot_status_list[-1:]
+        # else:
+            
+
         #for start event normal case last state = docked switched to autopilot and current events are available
-        if robot_status_list[0].lower() == 'docked':
-            if robot_status_list[1].lower() != 'autopilot' and start_date_event and start_time_event:
-                notification = Embed(description='**Start of event failed, robot state is '+robot_status_list[1]+'**',color=0xF04747)
+        if robot_status_list[-2].lower() == 'docked':
+            if robot_status_list[-1].lower() != 'autopilot' and start_date_event and end_date_event:
+                notification = Embed(description='** Failed Start event '+start_date_event+' '+start_time_event+' , Robot state is '+robot_status_list[-1]+'**',color=0xF04747)
                 discord_channel.send(embed=notification)
-
+            if robot_status_list[-1].lower() == 'autopilot' and start_date_event and end_date_event and len(list_state['current_events'])==1:
+                notification = Embed(description='** Success Start event '+start_date_event+' '+start_time_event+'**',color=0x5CDBF0)
+                discord_channel.send(embed=notification)
         #for end event normal case last state = autopilot switched to docking 
-        if robot_status_list[0].lower() == 'autopilot':
-            if  robot_status_list[1].lower() != 'docking' and len(list_state['current_events'])==0:
-                notification = Embed(description='**End of event failed, robot state is '+robot_status_list[1]+'**',color=0xF04747)
+        if robot_status_list[-2].lower() == 'autopilot':
+            if  robot_status_list[-1].lower() != 'docking' and len(list_state['current_events'])==0:
+                notification = Embed(description='** Failed End event , Robot state is '+robot_status_list[-1]+'**',color=0xF04747)
                 discord_channel.send(embed=notification) 
-
+            if  robot_status_list[-1].lower() == 'docking':
+                notification = Embed(description='** Success End event **',color=0x5CDBF0)
+                discord_channel.send(embed=notification) 
+        
+        #to get only the last 2 elements of robot_status_list to avoid having an array with unlimited content
+        robot_status_list=robot_status_list[-2:] 
        
+
 schedule.every(30).seconds.do(StartEndEvent)
     
 while True:
